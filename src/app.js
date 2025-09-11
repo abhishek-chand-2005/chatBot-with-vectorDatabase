ddabb88df7c2c848fe95b8c5c16590c28f9da9a0
Abhishek Chand <0101abhi0101@gmail.com>
2025-09-11 10:36:54 +0530

    update frontend code

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes)
 
 
-app.get("*name", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 module.exports = app;
